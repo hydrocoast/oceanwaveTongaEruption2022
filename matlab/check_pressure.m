84ac84fde947db8c971784c44a647fa3b5cb334e
Takuya Miyashita <miyashita@hydrocoast.jp>
2022-01-30 17:52:31 +0900

    rename

--- matlab/check_pressure.m
+++ matlab/check_pressure.m
@@ -12,13 +12,15 @@ savegif = 'test.gif';
 
 fig = figure;
 for k = 1:nt-3
-    clf(fig);
+    clf(fig);    
     ax = gca;
     ax.FontName = 'Helvetica';
-
     
     imagesc(lon,lat,pres(:,:,k)); ax.YDir = 'normal';
     axis equal tight
+    hold on
+    plot(lon0,lat0,'kp','MarkerFaceColor','y','MarkerSize',14)
+    hold off
     cb = colorbar('FontName','Helvetica');
     caxis([0,2]);
     title(sprintf('%d min',t(k)/60),'FontName','Helvetica')
@@ -41,4 +43,30 @@ for k = 1:nt-3
 end
 
 
-
+% fig = figure;
+% for k = 1:nt-3
+%     clf(fig);
+%     
+%     gx = geoaxes;
+%     geobasemap(gx,'colorterrain')
+%     geolimits(gx,latrange,lonrange)
+%     
+%     ax = axes;
+%     p = pcolor(lon,lat,pres(:,:,k)); shading flat
+%     p.FaceAlpha = 0.3;
+%     colormap(flipud(hot));
+%     axis tight
+%     hold on
+%     plot(lon0,lat0,'kp','MarkerFaceColor','y','MarkerSize',14)
+%     hold off
+%     caxis([0,2]);
+%     title(sprintf('%d min',t(k)/60),'FontName','Helvetica')    
+%     ax.Visible = 'off';
+%     ax.XTick = [];
+%     ax.YTick = [];    
+%     ax.Position = gx.Position;    
+%     print(gcf,'-djpeg',sprintf('step%03d.jpg',k));    
+% end
+% 
+% !ffmpeg -i step%03d.jpg -vf palettegen palette.png
+% !ffmpeg -r 2 -i step%03d.jpg -i palette.png -filter_complex paletteuse pres.gif
