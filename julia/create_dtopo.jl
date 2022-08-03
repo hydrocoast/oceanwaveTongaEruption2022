0c58d5eb00a08c50b677461aec19d30abc8b16c1
Takuya Miyashita <miyashita@hydrocoast.jp>
2022-08-03 17:45:38 +0900

    update dtopo

--- julia/create_dtopo.jl
+++ julia/create_dtopo.jl
@@ -1,13 +1,13 @@
 using VisClaw
 using Printf
 
-dx = dy = 0.25
-x = collect(Float64, 184.0:dx:185.0)
-#y = collect(Float64, -21.0:dy:-20)
-y = collect(Float64, 39.0:dy:40)
+dx = dy = 0.01
+x = collect(Float64, 184.59:dx:184.63)
+y = collect(Float64, -20.56:dy:-20.53)
+#y = collect(Float64, 0.00:dy:0.03)
 ny = length(y)
 nx = length(x)
-deform = ones(ny,nx)
+deform = 1000.0*ones(ny,nx)
 
 dtopo = VisClaw.DTopo(nx,ny,x,y,dx,dy,1,1.0,10.0,deform)
 
