f9cb417ed7e1cd0e79bef6c2a2d619faa4d126b1
Takuya Miyashita <miyashita@hydrocoast.jp>
2022-09-28 00:33:03 +0900

    update

--- sh_prepost/runclaw.sh
+++ sh_prepost/runclaw.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-make && make data && (make output | tee calc.log)
-(make juliaall | tee -a calc.log)  && ./creategif.sh
+make && make data && (make output 2>&1 | tee calc.log)
+(make juliaall 2>&1 | tee -a calc.log)  && ./creategif.sh
 make matlabplots
