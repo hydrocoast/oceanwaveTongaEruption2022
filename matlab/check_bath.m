bcfc6d1c12c0acff817f44706db9036839083830
Takuya Miyashita <miyashita@hydrocoast.jp>
2022-09-29 17:55:26 +0900

    update

--- matlab/check_bath.m
+++ matlab/check_bath.m
@@ -1,18 +1,43 @@
 clear
 close all
 
+%% read
+topofile = '../bathtopo/gebco_2022_n60.0_s-60.0_w110.0_e240.0.nc';
+[lon,lat,topo] = Topo.grdread2(topofile);
 
-topofile = './gebco_2021_n60.0_s-60.0_w120.0_e300.0.nc';
-
-[lon,lat,bath] = Topo.grdread2(topofile);
-
-
+%% bath
 figure
 ax = gca;
-imagesc(lon,lat,bath); ax.YDir  = 'normal';
+imagesc(lon,lat,topo); ax.YDir  = 'normal';
 axis equal tight
-demcmap([-5000,5000]);
+demcmap([-7000,4000]);
 cb = colorbar;
+
+%% wave celerity
+g = 9.8;
+c = -topo;
+c(c<0.0) = NaN;
+c = sqrt(g*c); % sqrt(g*h)
+
+[~,ind_west] = min(abs(lon-110.0));
+[~,ind_east] = min(abs(lon-155.0));
+[~,ind_south] = min(abs(lat-10.0));
+[~,ind_north] = min(abs(lat-50.0));
+
+lonj = lon(ind_west:ind_east);
+latj = lat(ind_south:ind_north);
+cj = c(ind_south:ind_north,ind_west:ind_east);
+
+
+figure;
+ax = gca;
+pcolor(lonj,latj,cj); shading flat
+axis equal tight
+colormap(ax,parula(15));
+caxis(ax,[170,320]);
+cb = colorbar;
+
+
 
 
 
