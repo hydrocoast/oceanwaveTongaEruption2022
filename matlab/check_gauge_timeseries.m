34d7b59555dd4af3d89d13a4ec99dab656b5ba8e
Takuya Miyashita <miyashita@hydrocoast.jp>
2022-11-15 01:51:30 +0900

    update

--- matlab/check_gauge_timeseries.m
+++ matlab/check_gauge_timeseries.m
@@ -1,9 +1,16 @@
 clear
 close all
 
+%% sim data
 simdir = '../testrun_kushimoto_5s/_output';
 list_gauge = dir(fullfile(simdir,'gauge*.txt'));
 ngauge = size(list_gauge,1);
+
+%% obs data
+obsdir = '~/Dropbox/miyashita/dataset/Tonga2022/IOC_Filtered_JPRUS_10_100';
+obsfile = 'Ftd_Kushimoto.txt';
+obsdat = readmatrix(fullfile(obsdir,obsfile),"FileType","text");
+
 
 g = cell(ngauge,1);
 for i = 1:ngauge
@@ -11,17 +18,24 @@ for i = 1:ngauge
     file = fullfile(simdir,list_gauge(i).name);
     
     dat = readmatrix(file,"FileType","text","NumHeaderLines",3);
-    dat(abs(dat(:,6))>10.0,6) = 0.0;
+    dat(abs(dat(:,6))>4.0, 6) = 0.0;
    
 
     g{i} = [dat(:,2),dat(:,6),dat(:,1)]; % time, eta, AMRlevel
 
 
     figure
-    plot(g{i}(:,1)./60,g{i}(:,2));
+    plot(g{i}(:,1)./3600,g{i}(:,2));
     grid on
     xlabel("Time (min)");
     ylabel("Water surface height (m)");
+    title("Kushimoto")
+
+    hold on
+    plot(obsdat(:,1)./3600,obsdat(:,2),'k-'); xlim([-1,13])
+
+    xline(0.0,'k-');
+    
     
 end
 
