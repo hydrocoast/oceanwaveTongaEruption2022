5273b48385e1301d2239876d69b5a66174251e9b
Takuya Miyashita <miyashita@hydrocoast.jp>
2022-11-24 15:30:05 +0900

    update

--- matlab/compare_timeseries_surf.m
+++ matlab/compare_timeseries_surf.m
@@ -2,16 +2,27 @@ clear
 close all
 
 %% sim data
-simdir = '../run_lamb/_output';
+simdir = '../run_lamb_ag/_output';
 list_gauge = dir(fullfile(simdir,'gauge*.txt'));
 ngauge = size(list_gauge,1);
 
-
 %% obs data
-load('IOC_JPRUS_surf.mat');
-% load('JMA_records.mat');
+load('JMA_records.mat');
 load('DART_records.mat');
 
+
+%% directory for export figs
+figdir = 'fig';
+option_printfig = 1; % 1: on, others: off
+if option_printfig == 1
+    if ~isfolder(figdir); mkdir(figdir); end
+    simcase_prefix = strrep(simdir,'/_output','');
+    [~,simcase_prefix] = fileparts(simcase_prefix);
+end
+
+
+
+%% read and compare
 g = cell(ngauge,1);
 for i = 1:ngauge
 % for i = 1:1
@@ -31,7 +42,7 @@ for i = 1:ngauge
     lat = header{3};
 
     %% find the closest gauge obs
-    [dist,ind_row] = min(sqrt((table_obs.Lat-lat).^2+(table_obs.Lon-lon).^2));
+    [dist,ind_row] = min(sqrt((table_JMA.Lat-lat).^2+(table_JMA.Lon-lon).^2));
     if dist>0.5
         ind_row = []; % not found
     end
@@ -51,18 +62,18 @@ for i = 1:ngauge
 
     %% plot
     figure
-    p1 = plot(g{i}(:,1)./3600,g{i}(:,2));
+    p1 = plot(g{i}(:,1)./3600,g{i}(:,2),'-','LineWidth',1);
     grid on
     xlabel("Time (min)",'FontName','Helvetica','FontSize',14);
     ylabel("Water surface height (m)",'FontName','Helvetica','FontSize',14);
 
     hold on
     if isdart
-        p2 = plot(cell2mat(table_DART.Time(ind_row))./3600, 1e-2*cell2mat(table_DART.Eta_filtered(ind_row)),'k-');
+        p2 = plot(cell2mat(table_DART.Time(ind_row))./3600, 1e-2*cell2mat(table_DART.Eta_filtered(ind_row)),'k-','LineWidth',1);
         title(sprintf('DART %05d',table_DART.DART(ind_row)),'FontName','Helvetica','FontSize',14);
     else
-        p2 = plot(cell2mat(table_obs.Time(ind_row))./3600, cell2mat(table_obs.Eta_filtered(ind_row)),'k-');
-        title(table_obs.Name(ind_row),'FontName','Helvetica','FontSize',14);
+        p2 = plot(cell2mat(table_JMA.Time(ind_row))./3600, 1e-2*cell2mat(table_JMA.Eta_filtered(ind_row)),'k-','LineWidth',1);
+        title(table_JMA.Name(ind_row),'FontName','Helvetica','FontSize',14);
     end
 
     xlim([-1,13])
@@ -70,6 +81,15 @@ for i = 1:ngauge
     hold off
 
     legend([p1,p2],{'Sim.','Obs.'},'FontName','Helvetica','FontSize',14,'Location','northwest');
-    
+
+    if option_printfig == 1
+        if isdart
+            figfile = [simcase_prefix,sprintf('_DART%05d.png',table_DART.DART(ind_row))];
+        else
+            figfile = [simcase_prefix,'_',char(table_JMA.Name(ind_row))];
+        end
+        exportgraphics(gcf,fullfile(figdir,figfile),'Resolution',300,'ContentType','image');
+        exportgraphics(gcf,strrep(fullfile(figdir,figfile),'.png','.pdf'),'ContentType','vector');
+    end
 end
 
