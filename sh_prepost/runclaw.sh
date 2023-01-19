ab85ebee44a20acaf68b185d6cabd73bd2fb4f18
Takuya Miyashita <miyashita@hydrocoast.jp>
2023-01-19 11:32:11 +0900

    update

--- sh_prepost/runclaw.sh
+++ sh_prepost/runclaw.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+if [ $# -ge 1 ] ; then
+    export OMP_NUM_THREADS=$1
+fi
+
 make && make data && (make output 2>&1 | tee calc.log) && \
 (make juliaall 2>&1 | tee -a calc.log) && ./creategif.sh
-#make matlabplots
+##make matlabplots
