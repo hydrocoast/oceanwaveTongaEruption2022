5e68e471a7238ca06b7173de6855b285030201ce
Takuya Miyashita <miyashita@hydrocoast.jp>
2023-01-27 00:53:00 +0900

    update sh

--- sh_prepost/qsub_runclaw.sh
+++ sh_prepost/qsub_runclaw.sh
@@ -8,6 +8,9 @@
 
 export OMP_NUM_THREADS=40
 
-./xgeoclaw 1>calc.log 2>&1
+#./xgeoclaw 1>calc.log 2>&1
+python /home/miyashita/Research/AMR/clawpack/clawutil/src/python/clawutil/runclaw.py xgeoclaw _output \
+       True None . False False None 1>calc.log 2>&1
 julia -e 'include("./setsave.jl"); include("./setplot.jl"); include("./setconvert.jl");'
 ./creategif.sh
+
