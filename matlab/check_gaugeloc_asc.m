e23f3c1a3c3ef96fd3ff54a4905dfb7977c957ff
Takuya Miyashita <miyashita@hydrocoast.jp>
2023-03-10 18:55:07 +0900

    new gauge set

--- matlab/check_gaugeloc_asc.m
+++ matlab/check_gaugeloc_asc.m
@@ -27,8 +27,17 @@ close all
 % topofile = '../bathtopo/M7023.asc';
 % gauge_lonlat = [142.1960, 27.0931]; % Chichijima
 % -----
-% topofile = '../bathtopo/depth_0090-01_zone08_lonlat.asc';
+% topofile = '../bathtopo/zone08_depth_0090-01_lonlat.asc';
 % gauge_lonlat = [138.2220, 34.6097]; % Omaezaki
+% -----
+% topofile = '../bathtopo/zone08_depth_0090-01_lonlat.asc';
+% gauge_lonlat = [137.6060, 34.6826]; % Maisaka
+% -----
+% topofile = '../bathtopo/zone08_depth_0090-02_lonlat.asc';
+% gauge_lonlat = [138.5160, 35.0146]; % Shimizu
+% -----
+% topofile = '../bathtopo/zone08_depth_0090-02_lonlat.asc';
+% gauge_lonlat = [138.8903, 35.0201]; % Uchiura
 % -----
 % topofile = '../bathtopo/depth_0090-03_zone01_lonlat.asc';
 % gauge_lonlat = [129.5370, 28.3229]; % Amami
@@ -69,6 +78,55 @@ close all
 % topofile = '../bathtopo/depth_0090-11_zone09_lonlat.asc';
 % gauge_lonlat = [140.8916, 36.9330]; % Onahama
 % -----
+% topofile = '../bathtopo/zone06_depth_0090-06_mask_lonlat.asc';
+% gauge_lonlat = [136.8800, 35.0896]; % Nagoya
+% -----
+% topofile = '../bathtopo/zone06_depth_0090-06_mask_lonlat.asc';
+% gauge_lonlat = [137.1900, 34.6035]; % Akabane
+% -----
+% topofile = '../bathtopo/zone06_depth_0090-06_mask_lonlat.asc';
+% gauge_lonlat = [136.8230, 34.4896]; % Toba
+% -----
+% topofile = '../bathtopo/zone04_depth_0090-05_lonlat.asc';
+% gauge_lonlat = [134.5922, 33.7687]; % Tokushima Yuki
+% -----
+% topofile = '../bathtopo/zone04_depth_0090-05_lonlat.asc';
+% gauge_lonlat = [134.5940, 34.0118]; % Tokushima Komatsushima
+% -----
+% topofile = '../bathtopo/zone04_depth_0090-05_lonlat.asc';
+% gauge_lonlat = [135.1420, 34.2188]; % Wakayama
+% -----
+% topofile = '../bathtopo/zone04_depth_0090-05_lonlat.asc';
+% gauge_lonlat = [135.1630, 33.8493]; % Gobo
+% -----
+% topofile = '../bathtopo/zone06_depth_0090-01_mask_lonlat.asc';
+% gauge_lonlat = [135.4270, 34.6549]; % Osaka
+% -----
+% topofile = '../bathtopo/zone06_depth_0090-01_mask_lonlat.asc';
+% gauge_lonlat = [135.1910, 34.6812]; % Kobe
+% -----
+% topofile = '../bathtopo/zone06_depth_0090-01_mask_lonlat.asc';
+% gauge_lonlat = [135.1781, 34.3389]; % Tanwa
+% -----
+% topofile = '../bathtopo/zone06_depth_0090-01_mask_lonlat.asc';
+% gauge_lonlat = [134.9011, 34.3467]; % Sumoto
+% -----
+% topofile = '../bathtopo/zone09_depth_0090-06_mask_lonlat.asc';
+% gauge_lonlat = [139.7700, 35.6486]; % Harumi
+% -----
+% topofile = '../bathtopo/zone02_depth_0090-06_lonlat.asc';
+% gauge_lonlat = [130.9644, 30.4636]; % Tanegashima
+% -----
+% topofile = '../bathtopo/zone02_depth_0090-07_lonlat.asc';
+% gauge_lonlat = [131.4060, 31.5757]; % Aburatsu
+% -----
+% topofile = '../bathtopo/zone02_depth_0090-10_lonlat.asc';
+% gauge_lonlat = [131.9600, 32.9507]; % Saiki Matsuura
+% -----
+topofile = '../bathtopo/zone02_depth_0090-10_lonlat.asc';
+gauge_lonlat = [132.5490, 33.2271]; % Uwajima
+% -----
+load('./JMA.mat');
 
 
 ngauge = size(gauge_lonlat,1);
