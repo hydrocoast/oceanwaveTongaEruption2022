d8ec85a58dd5f63ffa94f60607afae44cd8764ca
Takuya Miyashita <miyashita@hydrocoast.jp>
2024-04-01 11:48:18 +0900

    update sync config

--- sh_prepost/sync_from_server.sh
+++ sh_prepost/sync_from_server.sh
@@ -4,6 +4,10 @@ hostname="h100"
 simdir=`basename $(pwd)`
 rsync -av "miyashita@$hostname:Research/AMR/oceanwaveTongaEruption2022/$simdir/_output/gauge*.txt" _output/
 rsync -av "miyashita@$hostname:Research/AMR/oceanwaveTongaEruption2022/$simdir/_plots" .
+<<<<<<< Updated upstream
 rsync -av "miyashita@$hostname:Research/AMR/oceanwaveTongaEruption2022/$simdir/_mat" .
+=======
+rsync -av "miyashita@$hostname:Research/AMR/oceanwaveTongaEruption2022/$simdir/_grd" .
+>>>>>>> Stashed changes
 rsync -av "miyashita@$hostname:Research/AMR/oceanwaveTongaEruption2022/$simdir/_jld2" .
 
